34d167cefb5533231508253d3bb8ed7992d98c23
tharunV9053 <vanamalatharun1234@gmail.com>
2025-11-07 12:44:04 +0530

    v3 java

--- app.java
+++ app.java
@@ -4,3 +4,17 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+//dev2
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
